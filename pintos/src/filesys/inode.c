13eef9aef5780a03e168b6f0b604902ba1ec03c3
Yin Deng <yindeng@berkeley.edu>
2020-08-12 04:38:05 +0000

    finish task 2 without sync

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -496,42 +496,13 @@ inode_write_at (struct inode *inode, const void *buffer_, off_t size,
       if (chunk_size <= 0)
         break;
 
-      if (sector_ofs == 0 && chunk_size == BLOCK_SECTOR_SIZE)
-        {
-          /* Write full sector directly to disk. */
-          bufcache_write(sector_idx, buffer + bytes_written, 0, BLOCK_SECTOR_SIZE);
-          //block_write (fs_device, sector_idx, buffer + bytes_written);
-        }
-      else
-        {
-          /* We need a bounce buffer. */
-          if (bounce == NULL)
-            {
-              bounce = malloc (BLOCK_SECTOR_SIZE);
-              if (bounce == NULL)
-                break;
-            }
-
-          /* If the sector contains data before or after the chunk
-             we're writing, then we need to read in the sector
-             first.  Otherwise we start with a sector of all zeros. */
-          if (sector_ofs > 0 || chunk_size < sector_left)
-            bufcache_read(sector_idx, bounce, 0, BLOCK_SECTOR_SIZE);
-            //block_read (fs_device, sector_idx, bounce);
-          else
-            memset (bounce, 0, BLOCK_SECTOR_SIZE);
-          memcpy (bounce + sector_ofs, buffer + bytes_written, chunk_size);
-          
-          bufcache_write(sector_idx, bounce, 0, BLOCK_SECTOR_SIZE);
-          //block_write (fs_device, sector_idx, bounce);
-        }
+      bufcache_write(sector_idx, (void *)(buffer + bytes_written), sector_ofs, chunk_size);
 
       /* Advance. */
       size -= chunk_size;
       offset += chunk_size;
       bytes_written += chunk_size;
     }
-  free (bounce);
 
   return bytes_written;
 }
